48026f9bb5e5c1b3d711a6b7eaf2329ebaa3f99b
truongtrong99 <truongtrong400@gmail.com>
2025-07-10 14:06:18 +0700

    #53 - feat: add exceljs for user data import functionality
    - Added exceljs dependency to package.json.
    - Implemented file upload and parsing of Excel files in ImportUser component.
    - Extracted user data (fullName, email, address) from the uploaded Excel file and displayed it in a preview table.
    - Enabled import button only when data is available and cleared data on modal close.

--- src/components/admin/user/data/import.user.tsx
+++ src/components/admin/user/data/import.user.tsx
@@ -2,12 +2,20 @@
 import React from 'react';
 import { InboxOutlined } from '@ant-design/icons';
 import type { UploadProps } from 'antd';
-import { message, Table, Upload } from 'antd';
+import { App, message, Table, Upload } from 'antd';
 import { Modal } from 'antd';
-
+import { useState } from 'react';
+import Exceljs from 'exceljs';
+import { Buffer } from 'buffer';
 interface IProps {
     isOpenModalImport: boolean;
     setIsOpenModalImport: (isOpen: boolean) => void;
+}
+
+interface IDataImport {
+    fullName: string;
+    email: string;
+    address: string;
 }
 
 const { Dragger } = Upload;
@@ -15,20 +23,49 @@ const { Dragger } = Upload;
 
 const ImportUser = (props: IProps) => {
     const { isOpenModalImport, setIsOpenModalImport } = props;
-    ;
+
+    const { message } = App.useApp();
+    const [dataImport, setDataImport] = useState<IDataImport[]>([]);
 
     const uploadProps: UploadProps = {
         name: 'file',
         multiple: false,
         maxCount: 1,
         // action: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',
-        onChange(info) {
+        async onChange(info) {
             const { status } = info.file;
             if (status !== 'uploading') {
                 console.log(info.file, info.fileList);
             }
             if (status === 'done') {
                 message.success(`${info.file.name} file uploaded successfully.`);
+                if (info.fileList && info.fileList.length > 0) {
+                    const file = info.fileList[0].originFileObj!;
+                    const workbook = new Exceljs.Workbook();
+                    const arrayBuffer = await file.arrayBuffer();
+                    const buffer = Buffer.from(arrayBuffer);
+                    await workbook.xlsx.load(buffer);
+
+                    let jsonData: IDataImport[] = [];
+                    workbook.worksheets.forEach(function (sheet) {
+                        // read first row as data keys
+                        let firstRow = sheet.getRow(1);
+                        if (!firstRow.cellCount) return;
+                        let keys = firstRow.values as any;
+                        sheet.eachRow((row, rowNumber) => {
+                            if (rowNumber == 1) return;
+                            let values = row.values as any;
+                            let obj = {} as any;
+                            for (let i = 1; i < keys.length; i++) {
+                                obj[keys[i]] = values[i];
+                            }
+                            jsonData.push(obj);
+                        })
+
+                    });
+
+                    setDataImport(jsonData);
+                }
             } else if (status === 'error') {
                 message.error(`${info.file.name} file upload failed.`);
             }
@@ -53,12 +90,14 @@ const ImportUser = (props: IProps) => {
             onOk={() => setIsOpenModalImport(false)}
             onCancel={() => {
                 setIsOpenModalImport(false);
+                setDataImport([]); // Clear data on modal close
             }}
             okText="Import Data"
             okButtonProps={{
-                disabled: true
+                disabled: dataImport.length > 0 ? false : true
             }}
             maskClosable={false}
+            destroyOnClose={true}
         >
             <Dragger {...uploadProps}>
                 <p className="ant-upload-drag-icon">
@@ -73,6 +112,7 @@ const ImportUser = (props: IProps) => {
             <div style={{ paddingTop: 20 }}>
                 <Table
                     title={() => <span>Preview Data</span>}
+                    dataSource={dataImport}
                     columns={[
                         {
                             title: 'Full Name',
@@ -83,8 +123,8 @@ const ImportUser = (props: IProps) => {
                             dataIndex: 'email',
                         },
                         {
-                            title: 'Address',
-                            dataIndex: 'address'
+                            title: 'Phone',
+                            dataIndex: 'phone'
                         },
                     ]}
                 />
